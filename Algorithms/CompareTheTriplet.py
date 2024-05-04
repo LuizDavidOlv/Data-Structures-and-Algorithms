ff6baaaaabc83b72f0c4bffe11c89cbe18c944c2
David Oliveira <Luiz.Oliveira2@Dell.com>
2024-05-03 20:40:14 -0400

    Compare the Triplets function optimized

--- Algorithms/CompareTheTriplet.py
+++ Algorithms/CompareTheTriplet.py
@@ -1,32 +1,18 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'compareTriplets' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts following parameters:
-#  1. INTEGER_ARRAY a
-#  2. INTEGER_ARRAY b
-#
+# https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=true
 
 def compareTriplets(a, b):
+    aSum = 0
+    bSum = 0
     for i in range(len(a)):
         if a[i] > b[i]:
-            a += 1
+            aSum += 1
         elif b[i] > a[i]:
-            b += 1
-    return a, b
+            bSum += 1
+    return aSum, bSum
 
 
-a = list(map(int, input().rstrip().split()))
-
-b = list(map(int, input().rstrip().split()))
+a = [5, 6, 7]
+b = [3, 6, 10]
 
 result = compareTriplets(a, b)
 
