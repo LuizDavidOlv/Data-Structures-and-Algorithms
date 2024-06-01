42fd08f21d4f7016e3c458ab9646653c73ce2c4c
David Oliveira <Luiz.Oliveira2@Dell.com>
2024-06-01 12:08:20 -0400

    Refactor lastNonEmptyString function to use hash mapping

--- DataStructure/HashTables/LastNonEmptyString.py
+++ DataStructure/HashTables/LastNonEmptyString.py
@@ -1,15 +1,27 @@
 # https://leetcode.com/problems/apply-operations-to-make-string-empty/description/
 
-def lastNonEmptyString(s):
-    while s != "":
-        ocurrence = []
-        temp = ""
-        for char in s:
-            if(char not in ocurrence):
-                temp+=char
-                ocurrence.append(char)
-                s = s.replace(char, "",1)
-            if(s == ""):
-                return temp
+from typing import Counter
 
-print(lastNonEmptyString("aabcbbca"))
+class BetterSolution:
+    def lastNonEmptyString(s):
+        # Reversed so order of char keys in Counter will be from right to left in s.
+        # We want this ordering because chars will be removed from left to right, 
+        # so the remaining chars before the last operation will be the right-most ones 
+        # (i.e. the first chars we encounter when iterating right to left in s)
+        counts = Counter(reversed(s))
+
+        # only the chars with the max count will remain before the last operation
+        max_count = max(counts.values())
+        response = ""
+
+       # reverse again to get order of chars before last operation from left to right 
+        # reversedCounts = reversed(counts)
+        # for char in reversedCounts:
+        #     if counts[char] == max_count:
+        #         response += char
+
+        return "".join(char for char in reversed(counts) if counts[char] == max_count)
+
+
+if __name__ == '__main__':
+   print(BetterSolution.lastNonEmptyString("aabcbbcaad"))
