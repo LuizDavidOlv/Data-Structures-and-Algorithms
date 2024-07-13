88390850b084fc0ab17de2428d469957fb39e355
David Oliveira <Luiz.Oliveira2@Dell.com>
2024-07-13 16:11:06 -0400

    MergeTwoSortedLists example added

---
+++ DataStructure/LinkedList/MergeTwoSortedLists.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        current = dummy = ListNode()
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1, current = list1.next, list1
+            else:
+                current.next = list2
+                list2, current = list2.next, list2
+        
+        if list1 or list2:
+            current.next = list1 if list1 else list2
+        
+        return dummy.next
+    
+
+if __name__ == "__main__":
+    list1 = ListNode(1)
+    list1.next = ListNode(2)
+    list1.next.next = ListNode(4)
+
+    list2 = ListNode(1)
+    list2.next = ListNode(3)
+    list2.next.next = ListNode(4)
+
+    solution = Solution()
+    result = solution.mergeTwoLists(list1, list2)
+    print(result)
