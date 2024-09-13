1a18ea7e748e591d535fd98490f72a54dded7f48
David Oliveira <Luiz.Oliveira2@Dell.com>
2024-09-12 22:12:18 -0300

    H-index - Arrays

---
+++ DataStructure/Arrays/H_Index/H-index.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        citLength = len(citations)
+        hIndex = 0
+        remainingIndexes = citLength
+        for i in range(citLength): 
+            print(f'i: {i}')
+            if citations[i] >= remainingIndexes:
+                hIndex = remainingIndexes
+                break
+            remainingIndexes -= 1
+          
+        return hIndex
+            
+        
+        
+if __name__ == '__main__':
+    solution = Solution()
+    numList = [1,2]
+    result = solution.hIndex(numList)
+    print(result)
+
+   
+
+
+ 
