c1d8d2a702c701ed790512c967de2915ee7bf18d
David Oliveira <Luiz.Oliveira2@Dell.com>
2024-11-15 17:36:07 -0300

    Merge Intervals - Intervals

---
+++ DataStructure/Intervals/MergeIntervals.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        merged = []
+        intervals.sort(key= lambda x: x[0])
+
+        prev = intervals[0]
+
+        for interval in intervals[1:]:
+            if interval[0] <= prev[1]:
+                prev[1] = max(prev[1], interval[1])
+            else:
+                merged.append(prev)
+                prev = interval
+        
+        merged.append(prev)
+
+        
+        return merged
+
+if __name__ == '__main__':
+    solution = Solution()
+    inputList = [[2,3],[2,2],[3,3],[1,3],[5,7],[2,2],[4,6]]
+    result = solution.merge(inputList)
