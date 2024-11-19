542fc3abd1e9ec25d784705cbabd9801131f1a34
David Oliveira <Luiz.Oliveira2@Dell.com>
2024-11-18 22:27:42 -0300

    MoveZeros - TwoPointer

--- Algorithms/TwoPointers/MoveZeros.py
+++ Algorithms/TwoPointers/MoveZeros.py
@@ -1,11 +1,38 @@
-class Solution:
+from typing import List
+
+class Solution1:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
         left = 0
-        for right in range(len(nums)):
+        listSize = len(nums)
+
+        # until a zero value is found, left and right will be equal
+        # if a zero value apears, left will not increase
+        # this means that in the next iteration, the left value will be zero
+        # the swap will occur and zero will be on the right
+        for right in range(listSize):
             if nums[right] != 0:
                 nums[right], nums[left] = nums[left], nums[right]
                 left += 1
- 
+        
+class Solution2:
+    def moveZeroes(self, nums: List[int]) -> None:
+        write = 0
+        listSize = len(nums)
+
+        # as i traverse the array, write will keep constant until num is not zero
+        # This will make sure that all non zero integers are placed first
+        for i in range(listSize):
+            if nums[i] != 0:
+                # place non zero value after the last non zero value 
+                nums[write] = nums[i]
+                write += 1
+
+        # after traversin the array the left over value will still be wrong
+        # That is why it is needed to force them to become zero
+        nums[write:] = [0] * (listSize-write)
+
+
+if __name__ == '__main__':
+    solution = Solution1()
+    inputList = [7,1,0,3,12] 
+    result = solution.moveZeroes(inputList)
