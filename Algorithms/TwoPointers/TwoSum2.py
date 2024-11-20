b3ceb4f6d7dd462fa6988ee373455fc8d63f16ee
David Oliveira <Luiz.Oliveira2@Dell.com>
2024-11-20 18:15:09 -0300

    TwoSum2 better solution added

--- Algorithms/TwoPointers/TwoSum2.py
+++ Algorithms/TwoPointers/TwoSum2.py
@@ -6,19 +6,14 @@
 #* Two Pointers Solution
 class Solution:
     def twoSum(self, numbers: list[int], target: int) -> list[int]:
-        left, right = 0, len(numbers) - 1
-
-        while left < right:
-            two_sum = numbers[left] + numbers[right]
-
-            if two_sum == target:
-                return [left + 1, right + 1]
-            elif two_sum < target:
-                left += 1
-            else:
+        listSize = len(numbers)
+        right = listSize-1
+        for left in range(listSize):
+            while numbers[left] + numbers[right] > target:
                 right -= 1
-            
-        return [-1, -1]
+
+            if numbers[left] + numbers[right] == target:
+                return [left+1,right+1]
 
 
 
