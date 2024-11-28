1e7079ec851a9604fa0634e7cc7d14c5fc5da51e
David Oliveira <Luiz.Oliveira2@Dell.com>
2024-11-27 21:36:01 -0300

    Better Solution Added to LongestString -  Set

--- Algorithms/SlidingWindow/LongestString.py
+++ Algorithms/SlidingWindow/LongestString.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/description/?envType=study-plan-v2&envId=top-interview-150
 
-class Solution:
+class ListSolution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         substring = []
         s_list = list(s)
@@ -17,4 +17,27 @@ class Solution:
             if len(substring) > max_len:
                 max_len = len(substring)
 
-        return max_len      
+        return max_len
+
+class SetSolution():
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        sub = set()
+        left = 0
+        max_length = 0
+        list_size = len(s)
+
+        for right in range(list_size):
+            while s[right] in sub:
+                sub.remove(s[left])
+                left+=1
+                
+            
+            sub.add(s[right])
+
+            max_length = max( max_length, right-left+1)
+
+        return max_length
+
+
+if __name__ == '__main__':
+    result = SetSolution().lengthOfLongestSubstring('pwwkew')
