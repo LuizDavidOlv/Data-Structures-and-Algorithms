66a50d2050c05c1f2d85402b3f822542f633ec71
David Oliveira <Luiz.Oliveira2@Dell.com>
2024-11-30 14:12:28 -0300

    GroupAnagram better solution added

--- DataStructure/HashTables/GroupAnagrams.py
+++ DataStructure/HashTables/GroupAnagrams.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-
+#Time complexity O(N Log N)
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         anagram_map = defaultdict(list)
@@ -10,10 +10,26 @@ class Solution:
             anagram_map[sorted_word].append(word)
         
         return list(anagram_map.values())
+
+#Time Complexity O(N)
+class Solution2:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        anagram_dict = defaultdict(list)
+
+        for word in strs:
+            counts = [0]*26
+            for char in word:
+                counts[ord(char)- ord('a')] += 1
+            
+            key = tuple(counts)
+            anagram_dict[key].append(word)
+        
+        return list(anagram_dict.values())
+
     
 
 if __name__ == '__main__':
-    solution = Solution()
+    solution = Solution2()
     result = solution.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
     if ['eat', 'tea', 'ate'] in result and ['tan', 'nat'] in result and ['bat'] in result:
         print("Passed")
