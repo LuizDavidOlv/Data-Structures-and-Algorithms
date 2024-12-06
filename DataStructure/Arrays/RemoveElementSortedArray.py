cb3d0c87b35e1fa02a76597f480b8c360796c737
David Oliveira <Luiz.Oliveira2@Dell.com>
2024-12-06 11:56:47 -0300

    RemoveElementsSortedArray - Two Pointers - Array

--- DataStructure/Arrays/RemoveElementSortedArray.py
+++ DataStructure/Arrays/RemoveElementSortedArray.py
@@ -1,20 +1,11 @@
 #Source https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
-nums = [1,1]
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        write_index =1
 
-def removeElement(self, nums):
-    i = 0
-    while i< len(nums) -1:
-        if nums[i] == nums[i+1]:
-            nums.pop(i+1)
-        else:
-            i += 1
-    return len(nums), nums
-
-def removeElement2(self, nums):
-    j = 1
-    for i in range(1, len(nums)):
-        if nums[i] != nums[i - 1]:
-            nums[j] = nums[i]
-            j += 1
-    return j, nums[:j]
-print(removeElement2(removeElement, nums))
+        for read_index in range(1, len(nums)):
+            if nums[read_index] != nums[write_index-1]:
+                nums[write_index] = nums[read_index]
+                write_index += 1
+        
+        return write_index
